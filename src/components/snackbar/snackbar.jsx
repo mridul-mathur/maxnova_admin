24c0522c821fa3cdb23c0719c17ccbe1f95785dc
mukund <maheshwari31mukund@gmail.com>
2024-08-24 23:55:10 +0530

    changes all error handlings are done

---
+++ src/components/snackbar/snackbar.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import { Snackbar, Alert } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { hideSnackBar } from "src/redux/actions/snackbarAction";
+
+const SnackBar = () => {
+    const dispatch = useDispatch();
+    const { open, message, severity } = useSelector((state) => state.snackbar);
+
+    const handleClose = () => {
+        dispatch(hideSnackBar());
+    };
+
+    return (
+        <>
+            <Snackbar open={open} autoHideDuration={3000} onClose={() => handleClose()} anchorOrigin={{
+               vertical: 'top',
+               horizontal: 'center' 
+            }}>
+                <Alert
+                    onClose={() => handleClose()}
+                    severity={severity}
+                    variant="filled"
+                    sx={{ width: '100%' }}
+                >
+                    {message}
+                </Alert>
+            </Snackbar>
+        </>
+    )
+}
+
+
+export default SnackBar
